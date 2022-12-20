6376c1c2b90c9096bdc1ef37c816eb3fe1642d2a
Дмитрий Романенко <dmitrijromanenko@Mac-mini-Dmitrij-2.local>
2022-12-20 17:17:32 +0200

    getlocation logic rewrited for extension front

--- extensions/cookies/assets/cookies-popup.js
+++ extensions/cookies/assets/cookies-popup.js
@@ -1,190 +1,44 @@
-const cookies = document.getElementById('cookies-container');
-console.log(cookies)
-const shadow = cookies.attachShadow({mode: 'open'}); 
-const styleNode = document.createElement("style");
-console.log(styleNode)
+// const cookies = document.getElementById('cookies-container');
+// const shadow = cookies.attachShadow({mode: 'open'}); 
+// const styleNode = document.createElement("style");
 
-styleNode.textContent = `
-@import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap');
+// styleNode.textContent = ``;
 
-.cookies {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  max-width: 1272px;
-  min-height: 164px; 
+// shadow.appendChild(styleNode);
 
-  font-family: 'DM Sans', sans-serif;
+// const contentNode = document.createElement('div');
+// contentNode.innerHTML = ``
+// shadow.appendChild(contentNode);
 
-  position: fixed;
-  bottom: 10px; 
-  left: 0;
-  right: 0;
-  margin: 0 auto; 
-  padding: 52px 48px;
+function geoFindMe() {
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
 
-  background-color: #fff;
-  border: 1px solid #D9DBE9;
-  box-shadow: 0px 14px 42px rgba(8, 15, 52, 0.06);
-  border-radius: 2px;
+  mapLink.href = '';
+  mapLink.textContent = '';
 
-  z-index: 1000;
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
 
-  animation: move 1s linear 1;
-  transition: bottom 1s linear;
-}
-
-.cookies:focus {
-  animation: hide 1s linear 1;
-}
-
-@keyframes move {
-  from {
-    bottom: -200px;
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
   }
 
-  to {
-    bottom: 10px;
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if (!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
   }
 }
 
-@keyframes hide {
-  from {
-    bottom: 10px;
-  }
-
-  to {
-    bottom: -200px;
-  }
-}
-
-.cookies__img {
-  margin-right: 22px;
-  width: 70px;
-}
-
-.cookies__info {
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 30px;
-  color: #6F6C90;
-
-  margin-right: 106px;
-}
-
-.cookies__info a {
-  color: #FFC611;
-}
-
-.cookies__accept {
-  background: #FFC611;
-  box-shadow: 0px 3px 12px rgba(74, 58, 255, 0.18);
-  border-radius: 56px;
-  border: 0;
-
-  cursor: pointer;
-
-  width: 280px;
-  height: 54px;
-
-  margin-right: 20px;
-
-  color: #fff;
-  font-weight: 700;
-  font-size: 16px;
-  line-height: 18px;
-}
-
-.cookies__accept:hover {
-  background: #f09f36;
-}
-
-.cookies__decline {
-  background: #fff;
-  border: 1px solid #EFF0F7;
-  box-shadow: 0px 4px 8px rgba(74, 58, 255, 0.08);
-  border-radius: 56px;
-
-  cursor: pointer;
-
-  margin-right: 26px;
-  width: 140px;
-  height: 54px;
-}
-
-.cookies__decline:hover {
-  background: rgb(233, 231, 231);
-}
-
-.cookies__cross {
-  width: 14px;
-  transition: transform 0.5s;
-
-  cursor: pointer;
-}
-
-.cookies__cross:hover {
-  transform: scale(1.5);
-}
-
-@media (max-width: 1200px) {
-  .cookies {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    max-width: 640px;
-    gap: 10px;
-    padding: 20px;
-  }
-
-  .cookies__img,
-  .cookies__info,
-  .cookies__accept,
-  .cookies__decline {
-    margin: 0;
-  }
-}
-`;
-
-shadow.appendChild(styleNode);
-
-const bg = "{{ 'cookies.png' | asset_url | img_tag: 'logo', 'cookies__img' }}"
-console.log(bg)
-
-const contentNode = document.createElement('div');
-contentNode.innerHTML = `<div 
-class="cookies" 
-style="background-color: {{ block.settings.background }}" 
-id="cookies">
-{{ 'cookies.png' | asset_url | img_tag: 'logo', 'cookies__img' }} 
-
-<div class="cookies__info">
-  {{ block.settings.policy_text }}
-</div>
-
-<button 
-  onclick="hidePopup();"
-  class="cookies__accept" 
-  style="background-color: {{ block.settings.primary-btn-background }}; color: {{ block.settings.primary-btn-text-color }}"
->
-  {{ 'cookie-button.svg' | asset_url | img_tag: 'accept' }}
-  {{ block.settings.primary-btn-text }}
-</button>
-
-<button 
-  onclick="hidePopup();"
-  class="cookies__decline"
-  style="background-color: {{ block.settings.secondary-btn-background }}; color: {{ block.settings.secondary-btn-text-color }}"
->
-  {{ block.settings.secondary-btn-text }}
-</button>
-
-<div onclick="hidePopup();">
-  {{ 'cross.svg' | asset_url | img_tag: 'cross', 'cookies__cross' }}
-</div>
-</div>`;
-shadow.appendChild(contentNode);
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
 
 function hidePopup() {
   const cookies = document.getElementById('cookies');
--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -19,49 +19,6 @@ export default function App() {
   // See documentation for <Routes /> for more info
   const pages = import.meta.globEager("./pages/**/!(*.test.[jt]sx)*.([jt]sx)");
 
-  useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-          console.log('Latitude is', position.coords.latitude)
-          console.log('Longitude is', position.coords.longitude)
-        },
-        function(error) {
-          console.error("Error code - " + error.code + ':' + error.message)
-        })
-        console.log('Available')
-      } else {
-        console.log('Not Available');
-      }
-  }, [])
-  
-  function geoFindMe() {
-      const status = document.querySelector('#status');
-      const mapLink = document.querySelector('#map-link');
-  
-      mapLink.href = '';
-      mapLink.textContent = '';
-  
-      function success(position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.latitude;
-  
-        status.textContent = '';
-        mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-        mapLink.textContent = `latitude: ${latitude} °, longitude: ${longitude} °`;
-      }
-  
-      function error() {
-        status.textContent = 'Cannot receive your location';
-      }
-  
-      if (!navigator.geolocation) {
-        status.textContent = 'Geolocation is not supporting by your browser';
-      } else {
-        status.textContent = 'Receiving geolocation...';
-        navigator.geolocation.getCurrentPosition(success, error);
-      }
-  }
-
   return (
     <AppProvider i18n={enTranslations}>
 
@@ -70,14 +27,6 @@ export default function App() {
 
         <Card sectioned>
           <ChoiceGeoCountries></ChoiceGeoCountries>
-        </Card>
-        
-        <Card>
-          <div>
-            <button id="find-me" onClick={geoFindMe}>Show my location</button>
-            <p id="status"></p>
-            <a id="map-link" target="_blank"></a>
-          </div>
         </Card>
       </Page>
     </AppProvider>
